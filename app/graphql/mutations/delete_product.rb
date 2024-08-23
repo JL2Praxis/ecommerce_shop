b2a33692706dc1fb19ba4eb67b16f31747769c30
jayne <jayne@jaynedebijixingdiannao.local>
2024-08-23 14:03:44 +0800

    [Rails] create deleteProduct API

---
+++ app/graphql/mutations/delete_product.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# app/graphql/mutations/delete_product.rb
+module Mutations
+  class DeleteProduct < BaseMutation
+    include Auth
+
+    argument :id, ID, required: true
+
+    field :error, Types::ErrorType, null: true
+
+    def resolve(args)
+      product = Product.find_by(id: args[:id], shop_id: current_shop_id)
+
+      return error!(404, "Product with ID #{args[:id]} not found.", 'NOT_FOUND') if product.blank?
+
+      product.status = :deleted
+
+      error!(422, product.errors.full_messages.join(', '), 'INVALID') unless product.save
+
+      { }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,9 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_shop, mutation: Mutations::CreateShop
+    
     field :create_product, mutation: Mutations::CreateProduct
     field :update_product, mutation: Mutations::UpdateProduct
+    field :delete_product, mutation: Mutations::DeleteProduct
   end
 end
